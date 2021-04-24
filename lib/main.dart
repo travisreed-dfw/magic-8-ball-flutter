2f2fc8a657e7c2b472136816eaa035a7697f9777
Travis Reed <traviswreed2@gmail.com>
2021-04-24 12:23:26 -0500

    Completed project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.indigoAccent,
+          appBar: AppBar(
+            title: Text('Ask me anything'),
+            backgroundColor: Colors.indigo[900],
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballFace = 1;
+  int clickTimes = 0;
+
+  void changeBall() {
+    setState(() {
+      ballFace = Random().nextInt(5) + 1;
+      clickTimes++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: TextButton(
+          onPressed: () {
+            changeBall();
+            print(
+                'I got clicked ball number $ballFace, clicked $clickTimes times');
+          },
+          child: Image.asset(
+            'images/ball$ballFace.png',
+          ),
+        ),
+      ),
+    );
+  }
+}
